74a75731e81a3a19108380089b6fe67587a3227f
Srinija1370 <srinijamayoju@gmail.com>
2024-08-16 09:44:36 +0530

    updated footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,7 +9,6 @@ const Footer = () => {
                 <p>COPYRIGHT © 2022 HARRY CAR PARTS - ALL RIGHTS RESERVED.</p>
                 <p>POWERED BY GODADDY</p>
             </div>
-            <br />
             <div className="footer-disclaimer"><b>Disclaimer:</b> The content on harry'scarparts is for informational purposes only. We do not guarantee the accuracy or completeness of the information provided. Use the content at your own risk. Harry'scarparts is not liable for any damages or losses arising from the use of this site. Links to third-party websites are provided for convenience, and we are not responsible for their content. </div>
         </footer>
     );
